3ccdb79a029b1b4a0bb2461b591a57f8594a56e0
AngelS289 <angeliuxsp@gmail.com>
2025-11-12 21:16:14 -0600

    asd

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ const port = process.env.PORT || 5000
 const connectDB = require("./config/db")
 const colors = require("colors")
 const { errorHandler } = require("./middleware/errorMiddleware")
+const cors = require("cors")
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
