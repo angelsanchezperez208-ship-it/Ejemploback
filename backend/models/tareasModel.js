2df80037984e65ee9c9950fc0c3ae4e2e4f2f612
AngelS289 <angeliuxsp@gmail.com>
2025-11-12 21:13:07 -0600

    pene

---
+++ backend/models/tareasModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const tareasSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+
+    },
+
+    texto: {
+        type: String,
+        required: [true, 'Por favor teclea el texto de la tarea']
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Tarea', tareasSchema)
